c59dc5c2dd5643708c3d24042d6bb821dbac9b5f
Rushabhsoni1306 <64094208+Rushabh1306@users.noreply.github.com>
2021-07-18 12:53:16 +0530

    Updated JS variables

---
+++ JSBasics.js
@@ -0,0 +1,20 @@
+//Variables Declaration ways -->var, let and const
+
+var a = 20
+var a = 30 //redeclaration allowed
+console.log(a)
+
+a = 60 //reupdation allowed
+console.log(a)
+
+let b = 30
+// let b = 37 //redeclaration is not allowed
+// console.log(b)
+
+b = 48 // updation is allowed
+console.log(b)
+
+const c = 20
+// const c = 30 //re-declaration is not allowed
+c = 33
+console.log(c)
